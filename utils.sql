6bdeea58845c5b7ed201e7d0cdf6eff532c3c7cf
Juan Pablo <122913492+JPGomezCuervo@users.noreply.github.com>
2025-03-20 16:00:44 -0500

    add utils.sql

---
+++ utils.sql
@@ -0,0 +1,19 @@
+INSERT INTO commissions (
+        commission_value,
+        commission_factor) VALUES (0.10, 10.0);
+
+INSERT INTO commissions (
+        commission_value,
+        commission_factor) VALUES (0.15, 6.60);
+
+INSERT INTO commissions (
+        commission_value,
+        commission_factor) VALUES (0.20, 5.0);
+
+INSERT INTO commissions (
+        commission_value,
+        commission_factor) VALUES (0.35, 2.85);
+
+INSERT INTO commissions (
+        commission_value,
+        commission_factor) VALUES (0.40, 1.80);
